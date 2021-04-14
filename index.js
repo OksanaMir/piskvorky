d838ddf8c1156fe624bf95706f9ba6f16e66f02d
Oksana Mironova <kq3hip13z@gmail.com>
2021-04-14 12:48:30 +0200

    After code review fexes and final version of part 4

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const btns = document.querySelectorAll('button');
 btns.forEach((btn) =>
   btn.addEventListener('click', (e) => {
     if (btn.disabled) {
-      return;
       console.log('cant play');
-    } else {
-      if (activePlayer == 'circle') {
-        e.target.className = 'board__field--circle';
-        //btn.style.backgroundImage = 'images/circle.svg';
-        document.querySelector('.game').innerHTML =
-          'HRAJE: <img id="cross" src="images/cross.svg" alt="cross" />';
-
-        //btn.disabled = true;
-        activePlayer = 'cross';
-      } else {
-        e.target.className = 'board__field--cross';
-        //btn.style.backgroundImage = 'images/cross.svg';
-        activePlayer = 'circle';
-        document.querySelector('.game').innerHTML =
-          'HRAJE: <img id="circle" src="images/circle.svg" alt="circle" />';
-
-        //btn.disabled = true;
-        activePlayer = 'circle';
-      }
-      btn.disabled = true;
+      return;
     }
+
+    if (activePlayer == 'circle') {
+      e.target.className = 'board__field--circle';
+      //btn.style.backgroundImage = 'images/circle.svg';
+      document.querySelector('.game').innerHTML =
+        'HRAJE: <img id="cross" src="images/cross.svg" alt="cross" />';
+
+      //btn.disabled = true;
+      activePlayer = 'cross';
+    } else {
+      e.target.className = 'board__field--cross';
+      //btn.style.backgroundImage = 'images/cross.svg';
+      activePlayer = 'circle';
+      document.querySelector('.game').innerHTML =
+        'HRAJE: <img id="circle" src="images/circle.svg" alt="circle" />';
+
+      //btn.disabled = true;
+      activePlayer = 'circle';
+    }
+    btn.disabled = true;
   }),
 );
